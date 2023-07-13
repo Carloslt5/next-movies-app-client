d13333321b5a09557f5fdfc2ada38b3a5a500ef5
Carloslt <carloslt.proyectos@gmail.com>
2023-07-13 12:53:38 +0200

    add uppercase details

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <>
-      <div className="container-fluid px-5">
+      <div className="container-fluid px-4 py-3">
         <h1>HOME</h1>
         <hr />
         <Link href={'/movies'} className='btn btn-dark'>View Movies</Link>
--- src/components/DetailsMovie/DetailsMovie.jsx
+++ src/components/DetailsMovie/DetailsMovie.jsx
@@ -2,7 +2,7 @@
 const DetailsMovie = ({ _id, title, poster, fullplot, imdb, directors, writers }) => {
     return (
         <>
-            <h1>{title}</h1>
+            <h1 style={{ textTransform: 'uppercase' }}>{title}</h1>
             <hr />
             <div className="row">
                 <div className="col-12 col-md-3 ">
@@ -12,22 +12,18 @@ const DetailsMovie = ({ _id, title, poster, fullplot, imdb, directors, writers }
                 </div>
                 <div className="col-12 col-md-9">
                     <div className="card-body">
-                        <h5 className="card-title mt-3 mt-sm-0">{title}</h5>
-                        <hr />
-                        <div>
-                            <article className="card-text" style={{ height: '100%' }}>
-                                <p><strong>Description:</strong></p>
-                                <p>{fullplot}</p>
-                                {
-                                    directors?.map(el => {
-                                        return (
-                                            <p><strong>Directors:</strong> {el}</p>
-                                        )
-                                    })
-                                }
-                                <strong>Rating:</strong> {imdb.rating}
-                            </article>
-                        </div>
+                        <article className="card-text" style={{ height: '100%' }}>
+                            <p><strong>Description:</strong></p>
+                            <p>{fullplot}</p>
+                            {
+                                directors?.map(el => {
+                                    return (
+                                        <p><strong>Directors:</strong> {el}</p>
+                                    )
+                                })
+                            }
+                            <strong>Rating:</strong> {imdb.rating}
+                        </article>
                     </div>
                 </div>
             </div >
