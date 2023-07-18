9ae33a2b7e4061c1f489c84fc7c794305441083e
Carloslt <carloslt.proyectos@gmail.com>
2023-07-18 11:26:49 +0200

    add navigation component

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import './style/globals.css'
-import Link from "next/link"
+import Navigation from '@/components/Navigation/Navigation'
 
 export const metadata = {
   title: 'Create Next App',
@@ -15,29 +15,7 @@ export default function RootLayout({ children }) {
       </head>
       <body suppressHydrationWarning={true}>
 
-        <nav className="navbar navbar-expand-lg bg-body-tertiary px-3">
-          <div className="container-fluid">
-            <Link href={'/'} className="navbar-brand">MOVIES APP</Link>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
-              <ul className="navbar-nav gap-2">
-
-                <li className="nav-item">
-                  <Link href={'/'}>HOME</Link>
-                </li>
-                <li className="nav-item">
-                  <Link href={'/movies'}>MOVIES</Link>
-                </li>
-                <li className="nav-item">
-                  <Link href={'/about'}>ABOUT</Link>
-                </li>
-
-              </ul>
-            </div>
-          </div>
-        </nav>
+        <Navigation />
 
         {children}
       </body>
